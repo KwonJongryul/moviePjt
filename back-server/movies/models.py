9e1150ad64be4396116b6974b5348838a32ab0ec
yunji <yunji5060@naver.com>
2023-05-18 16:18:02 +0900

    db 끝git add .

--- back-server/movies/models.py
+++ back-server/movies/models.py
@@ -10,20 +10,20 @@ class Genre(models.Model):
     
 # Create your models here.
 class Movie(models.Model):
-    adult = models.BooleanField()
+    adult = models.BooleanField(null=True)
     backdrop_path = models.TextField(null=True)
     genre = models.ManyToManyField(Genre, related_name='genre_movie')
-    id = models.IntegerField(primary_key=True)
-    original_language = models.CharField(max_length=10)
-    original_title = models.TextField()
-    overview = models.TextField()
-    popularity = models.FloatField()
+    # id = models.IntegerField(primary_key=True)
+    original_language = models.CharField(max_length=10, null=True)
+    original_title = models.TextField(null=True)
+    overview = models.TextField(null=True)
+    popularity = models.FloatField(null=True)
     poster_path = models.TextField(null=True)
-    release_date = models.DateField()
-    title = models.TextField()
-    video = models.BooleanField()
-    vote_average = models.FloatField()
-    vote_count = models.IntegerField()
+    release_date = models.DateField(null=True)
+    title = models.TextField(null=True)
+    video = models.BooleanField(null=True)
+    vote_average = models.FloatField(null=True)
+    vote_count = models.IntegerField(null=True)
     
     def __str__(self):
         return self.title
@@ -31,11 +31,11 @@ class Movie(models.Model):
 class Actor(models.Model):
     adult = models.BooleanField()
     gender = models.IntegerField()
-    known_for = models.TextField()
     department = models.CharField(max_length=20)
     name = models.CharField(max_length=30)
     popularity = models.FloatField()
     profile_path = models.TextField(null=True)
+    known = models.ManyToManyField(Movie, related_name='actor_movie')
     
     def __str__(self):
         return self.name
